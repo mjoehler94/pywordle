e27a92f06452a19406e91dbee8ee6a811640c88f
Matt Oehler <mjoehler94@gmail.com>
2022-02-17 21:19:53 -0700

    the game works! (no coloring yet though)

--- app.py
+++ app.py
@@ -6,29 +6,29 @@ from wordle import Wordle
 
 def main():
 
-    print("Welcome to PyWordle! \U0001f600  \U0001F40D")
-    word_length_prompt = "Enter your desired word length: "
-    while True:
-        try:
-            # wl = input(word_length_prompt)
-            wl = "5"
-            if 5 <= int(wl) <= 10:
-                break
-            else:
-                print("Word length must be between 5 and 10 (inclusive")
-        except Exception as e:
-            print(f"Invalid entry: {wl}")
-            print("Value must be an integer between 5 and 10 inclusive")
+    # while True:
+    #     try:
+    #         # wl = input(word_length_prompt)
+    #         wl = "5"
+    #         if 5 <= int(wl) <= 10:
+    #             break
+    #         else:
+    #             print("Word length must be between 5 and 10 (inclusive")
+    #     except Exception as e:
+    #         print(f"Invalid entry: {wl}")
+    #         print("Value must be an integer between 5 and 10 inclusive")
 
-    # initialize dictionary
-    with open("word_files/wordle_dictionary.txt", 'r') as f:
-        wordle_dict = [x for x in f.read().split()]
-    # with open("words_of_length.json", "r") as f:
-    #     word_bank = json.load(f)[wl]
-    # print(len(word_bank))
+    # initialize dictionaries
+    # with open("word_files/wordle_guess_dictionary.txt", 'r') as f:
+    #     guess_dict = [x for x in f.read().split()]
+    with open("word_files/words_of_length.json", "r") as f:
+        guess_dict = json.load(f)['5']
+
+    with open("word_files/wordle_game_dictionary.txt", 'r') as f:
+        game_dict = [x for x in f.read().split()]
 
     # initialize wordle object
-    game = Wordle(dictionary=wordle_dict, word_len=int(wl))
+    game = Wordle(guess_dictionary=guess_dict, game_dictionary=game_dict)
 
     # start game
     game.play_game()
--- wordle.py
+++ wordle.py
@@ -7,14 +7,15 @@ import blessings
 
 
 class Wordle:
-    def __init__(self, dictionary: list, game_words=None, word_len: int = 5) -> None:
+    def __init__(self, guess_dictionary: list, game_dictionary=None) -> None:
         # TODO: consider making dictionary a dict to improve performance
-        self.dictionary = dictionary
-        self.word_len = word_len
+        self.guess_dictionary = guess_dictionary
+        self.game_words = game_dictionary if game_dictionary is not None else guess_dictionary
+        self.game_word = random.choice(self.game_words).lower()
+        self.word_len = len(self.game_word)
         self.max_guesses = self.determine_guess_count()
-        self.current_guess = 1
+        self.current_guess_count = 0
         self.board = ["- " * self.word_len] * self.max_guesses
-        self.game_word = random.choice(self.word_bank)
         self.is_game_over = False
         self.guessed_words = []
 
@@ -33,11 +34,13 @@ class Wordle:
         """
         while True:
             prompt = "Enter your guess: "
-            word_to_guess = input(prompt)
+            word_to_guess = input(prompt).lower()
             if len(word_to_guess) != self.word_len:
                 print(f"Invalid Entry: Guess must be {self.word_len} characers")
-            elif word_to_guess not in self.word_bank:
+            elif word_to_guess not in self.guess_dictionary:
                 print("Invalid Guess: Guess is not in dictionary")
+            elif word_to_guess in self.guessed_words:
+                print(f"'{word_to_guess}' has already been guessed")
             else:
                 break
         return word_to_guess
@@ -48,17 +51,28 @@ class Wordle:
         update guess count
         check if game is over
         """
+        # add word to board
+        self.board[self.current_guess_count] = ''.join([letter.upper() + ' ' for letter in guess])
+        # update guess count and guessed words list
+        self.current_guess_count += 1
+        self.guessed_words.append(guess)
+        return
+
+    def display_board(self):
+        """use blessings module to display board with color hints
+        """
+        print(f"Game word is: {self.game_word}")  #TODO: remove after dev is complete
+        print(*self.board, sep="\n")
+        print()
         pass
 
-    def is_valid_word(self, word):
-        return word in self.dictionary
-
     def play_game(self):
+        print("Welcome to PyWordle! \U0001f600  \U0001F40D")
+
         while not self.is_game_over:
+
             # display board
-            print(f"Game word is: {self.game_word}")
-            print(*self.board, sep="\n")
-            print()
+            self.display_board()
 
             # prompt for next guess
             guess = self.get_guess()
@@ -66,15 +80,14 @@ class Wordle:
             # update board
             self.update_board(guess)
 
-            # display board
-
             # check if game is over
-
-            if self.is_game_over:
-                break
-
-        if self.max_guesses:
-            print("You won!")
+            if self.current_guess_count == self.max_guesses or guess.lower() == self.game_word:
+                self.is_game_over = True
+        
+        if self.game_word in self.guessed_words:
+            print("You won :)")
         else:
             print("You lost :(")
+
+            
         return
